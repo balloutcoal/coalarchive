8289aea42019217f379e63fc1f0f46a0c5c321d5
balloutcoal <thorpcol000@gmail.com>
2024-12-02 13:41:51 -0700

    post thanksgiving

--- USU/FALL24/ece1410/programs/program10/Sorts.hpp
+++ USU/FALL24/ece1410/programs/program10/Sorts.hpp
@@ -19,11 +19,29 @@ class Sorts
 
 // IMPLEMENTATION --- YOUR CODE
 
-void Sorts::bubble(int*, int){
+void Sorts::bubble(int* arr, int n){
+  for(int i = 0; i < n - 1; i++){
+    for(int j = 0; j < n - i - 1; j++){
+      if(arr[j] > arr[j+1]){
+        int temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
   return;
 }
 
-void Sorts::insertion(int*, int){
+void Sorts::insertion(int* arr, int n){
+  for(int i = 1; i < n; i++){
+    int curr = arr[i];
+    int j = i - 1;
+    while(j >= 0 && arr[j] > curr){
+      arr[j+1] = arr[j];
+      j = j - 1;
+    }
+    arr[j+1] = curr;
+  }
   return;
 }
 
